1162a0343e9c844426c1bc5ec20093549755b0fa
kahmadliflemingca <114246896+kahmadliflemingca@users.noreply.github.com>
2022-12-09 12:48:48 -0500

    Add files via upload

---
+++ javascript/README.md
@@ -0,0 +1,2 @@
+## JS Folder
+This folder is the directory for all of the JavaScript functions used in our a3web page.
--- javascript/a3webJavaS.js
+++ javascript/a3webJavaS.js
@@ -5,26 +5,26 @@
 //           https://flexiple.com/javascript/javascript-clock/
 //           https://developer.mozilla.org/en-US/docs/Web/CSS/border-radius
 //            https://www.w3schools.com/jsref/event_onclick.asp
-// Creating function to manipulate Docoument objects from W3 school when a button is clicked -AK//
-// Kept same format, mainpulating the ID's that it interacts with, and nested with clock function//
+// Creating function to manipulate document objects from W3 school when a button is clicked -AK//
+// Kept same format, manipulating the ID's that it interacts with, and nested with clock function//
 // so that the button causes both the date / time items and the text to appear within the stylized div container //
 /////////////////////////////////////////////////////////////////
   
 
 //initiate function called in HTML in the Clock holder -AK//
 function myFunction() {
-  document.getElementById("p2").innerHTML = "There's lots to explore.."; /// The text we want inserted on the button click -AK///
-  document.getElementById("p3").innerHTML = "But don't lose track of time!";
+  document.getElementById("p2").innerHTML="There's lots to explore.."; /// The text we want inserted on the button click -AK///
+  document.getElementById("p3").innerHTML="But don't lose track of time!";
   
-  /// Variable reinitiates the myTimer function every second so the clock keeps updating -AK///
-  var myVar = setInterval(function() {
+  /// Variable re-initiates the myTimer function every second so the clock keeps updating -AK///
+  var myVar=setInterval(function() {
   myTimer();
 }, 1000);
 
  /// Function adds the date methods to the identified IDs in the HTML ///
 function myTimer() {
-  var timeVariable = new Date(); /// Identifies the date and time in a long string to feed to other sections of the code ///
-  document.getElementById("clock").innerHTML = timeVariable.toLocaleTimeString(); /// Breaks down the date method and takes just the local time ///
-  document.getElementById("showDate").innerHTML = timeVariable.toLocaleDateString(); /// Breakds down the date method and takes just the date ////
+  var timeVariable=new Date(); /// Identifies the date and time in a long string to feed to other sections of the code ///
+  document.getElementById("clock").innerHTML=timeVariable.toLocaleTimeString(); /// Breaks down the date method and takes just the local time ///
+  document.getElementById("showDate").innerHTML=timeVariable.toLocaleDateString(); /// Breaks down the date method and takes just the date ////
 }
 }
